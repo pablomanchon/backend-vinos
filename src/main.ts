09d7234c1bb96e22fd9e4fe5c40d0ee282946c2f
Pablo Manchon <pablomanchon97@gmail.com>
2023-10-12 13:28:13 -0300

    cambio CORS

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://we-wines.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
