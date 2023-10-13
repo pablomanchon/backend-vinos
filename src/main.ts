e0e57319b55a19e697045fdf1f93b87005cce9ff
Pablo Manchon <pablomanchon97@gmail.com>
2023-10-13 10:46:09 -0300

    actualizo

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app: INestApplication = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
 
   app.enableCors({
     origin: 'https://we-wines.vercel.app',
@@ -15,4 +19,5 @@ async function bootstrap() {
 
   await app.listen(3000);
 }
+
 bootstrap();
